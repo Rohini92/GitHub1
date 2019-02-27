435d97a66e76d0e123f41b4ccfd1b8c37fc880a2
Rohini92 <rohi.srimathi02@gmail.com>
2019-02-26 19:05:58 -0500

    Second Commit

--- src/org/git/Base.java
+++ src/org/git/Base.java
@@ -4,7 +4,7 @@ public class Base {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("My First Prject");
+System.out.println("My Selenium First Prject");
 	}
 
 }
